da3993ce4417ebff4bdac1f068aaff50573cb5f4
Sean McArthur <sean.monstar@gmail.com>
2013-05-22 12:21:46 -0700

    start certifier in post_deploy

---
+++ scripts/awsbox/post_deploy.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+var fs = require('fs'),
+    child_process = require('child_process');
+
+var certifierConfig = '/home/app/code/node_modules/browserid-certifier/config/local.json';
+var configDist = '/home/app/code/node_modules/browserid-certifier/config/local.json-dist';
+
+process.stdout.write('Deploying config and public key\n');
+fs.writeFileSync(certifierConfig, fs.readFileSync(configDist));
+process.stdout.write('Updated ' + configDist + '\n');
+
+process.stdout.write('Compressing CSS and JS\n');
+var comp = child_process.spawn('/home/app/code/scripts/compress',
+                                   [], {cwd: '/home/app/code'});
+comp.stdout.pipe(process.stdout);
+comp.stderr.pipe(process.stderr);
+
+process.stdout.write('Starting up certifier\n');
+var certEnv = Object.create(process.env, {
+  CONFIG_FILES: certifierConfig
+});
+var cert = child_process.spawn('forever -a -l /home/app/var/log/certifier.log start /home/app/code/node_modules/browserid-certifier/bin/certifier', [], {
+  env: certEnv
+});
+
+cert.stdout.pipe(process.stdout);
+cert.stderr.pipe(process.stderr);
